3e0608bef35fdc70475a503c60c1fa6310fd6ebb
Kore Nordmann <mail@kore-nordmann.de>
2012-07-05 20:41:26 +0200

    Implemented: Simple countdown

--- src/com/kore/instructor/MyFirstActivity.java
+++ src/com/kore/instructor/MyFirstActivity.java
@@ -1,7 +1,10 @@
 package com.kore.instructor;
 
 import android.app.Activity;
+import android.view.View;
+import android.widget.TextView;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 
 public class MyFirstActivity extends Activity
 {
@@ -11,5 +14,30 @@ public class MyFirstActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    public void startCountdown(View view)
+    {
+       final TextView countdown = (TextView) findViewById(R.id.countdown);
+
+       new CountDownTimer(5000, 100) {
+            public void onTick(long remaining)
+            {
+                countdown.setText(
+                    String.format(
+                        "%02d:%02d:%02d.%02d",
+                        remaining / 1000 / 60 / 60 % 24,
+                        remaining / 1000 / 60 % 60,
+                        remaining / 1000 % 60,
+                        remaining / 10 % 100
+                    )
+                );
+            }
+
+            public void onFinish()
+            {
+                countdown.setText("00:00:00.00");
+            }
+       }.start();
     }
 }
